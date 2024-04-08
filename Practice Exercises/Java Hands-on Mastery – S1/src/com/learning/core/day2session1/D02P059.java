547316378f2274134b11404c01ed7c036fca6810
Mallikavempatapu <97509474+Mallikavempatapu@users.noreply.github.com>
2024-04-08 19:20:30 +0530

    Commit

---
+++ Practice Exercises/Java Hands-on Mastery – S1/src/com/learning/core/day2session1/D02P059.java
@@ -0,0 +1,17 @@
+package com.learning.core.day2session1;
+
+import java.util.HashSet;
+
+import java.util.Set;
+ 
+ 
+public class D02P059 {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Mr John Smith";
+		System.out.println(s.replace(" ","%20"));
+ 
+	}
+ 
+}
